bf323be92a42ed17f4848a53022bc16f991554fd
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 13:46:11 +0800

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from "./Home/Home"
 export { default as Login} from "./Login/Login"
 export { default as Register } from "./Register/Register"
+export { default as CreateRecipe } from "./CreateRecipe/CreateRecipe"
+export { default as SavedRecipes } from "./SavedRecipes/SavedRecipes"
