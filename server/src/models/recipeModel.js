90c417fc3699bb011e1f7d2e6c2042bc50a40f44
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-16 07:46:29 +0800

    created a model for recipe

---
+++ server/src/models/recipeModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  instructions: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  cookingTime: { type: Number, required: true },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+const recipeModel = mongoose.model("recipe", recipeSchema);
+
+export default recipeModel;
