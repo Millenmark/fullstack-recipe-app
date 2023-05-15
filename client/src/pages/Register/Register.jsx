cac5fa318480ed38737353f38f507dfdd2b8a9fa
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 13:42:23 +0800

    Update Register.jsx

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,32 @@
-
-
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <h2>Register Now</h2>
+      <form>
+        <div className="inputField">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter your username (must be unique)"
+          />
+        </div>
 
-export default Register
+        <div className="inputField">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button>Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
