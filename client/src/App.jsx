1f8ce6a0dc16009b66b7dcd75b09d9dcff987aa4
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-16 07:33:52 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable no-unused-vars */
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts";
 import { Home, Login, Register } from "./pages";
+import { useCookies } from "react-cookie";
 import "./App.css";
 
 function App() {
+  const [cookies, setCookies] = useCookies(["access_token"])
+
+  const isValidCookie = cookies.access_token ? true : false
+
   return (
     <>
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={isValidCookie ? <Home/> : <Login/>}/>
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
         </Route>
