8c090bd989dc10fd7db0715c996d350ef113d975
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-16 07:33:55 +0800

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
-import { Link } from "react-router-dom";
-import style from "./Navbar.module.css"
+import { Link, useNavigate } from "react-router-dom";
+import style from "./Navbar.module.css";
+import {useCookies} from "react-cookie"
+
 
 const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"])
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    setCookies("access_token", "")
+    localStorage.removeItem("userID")
+    navigate("/login")
+  }
+
   return (
     <nav className={style.nav}>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      {cookies.access_token ? (
+        <>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
     </nav>
   );
 };
