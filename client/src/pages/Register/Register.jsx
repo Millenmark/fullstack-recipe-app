e71541747c09da66ded7ae213fd96b3164779180
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 14:31:22 +0800

    Update Register.jsx

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,29 +1,52 @@
+import { useState } from "react";
+import axios from "axios"
+
 const Register = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+       await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, {
+        username,
+        password
+      });
+      alert("Registration Completed")
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
+  }
+
   return (
     <>
       <h2>Register Now</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="inputField">
-          <label>Username</label>
+          <label>Username: </label>
           <input
             type="text"
             name="username"
             id="username"
             placeholder="Enter your username (must be unique)"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
 
         <div className="inputField">
-          <label>Password</label>
+          <label>Password: </label>
           <input
             type="password"
             name="password"
             id="password"
             placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
-        <button>Register</button>
+        <button type="submit">Register</button>
       </form>
     </>
   );
