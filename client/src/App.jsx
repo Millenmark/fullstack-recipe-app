3c0060f7e5d737ebdf57231bfd73cdc40301c5db
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 13:31:18 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { DefaultLayout } from "./layouts";
+import { Home, Login, Register } from "./pages";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
