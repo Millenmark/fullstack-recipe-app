4ed61e0afefcd19aadcf0ac88cdfa94f081c1778
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 15:06:40 +0800

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,13 +1,39 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
+import {useNavigate} from "react-router-dom"
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [cookies, setCookies] = useCookies(["access_token"])
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        {
+          username,
+          password,
+        }
+      );
+      setCookies("access_token", response.data.token);
+      localStorage.setItem("userID", response.data.userID)
+      navigate("/")
+    } catch (error) {
+      const errorMessage = error?.response?.data?.message;
+      alert(errorMessage);
+    }
+  };
 
   return (
     <>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="inputField">
           <label>Username: </label>
           <input
