6d9682ea58ac132019040db82aa48c35eee5b040
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 12:50:28 +0800

    Update userController.js

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,8 +1,22 @@
 import jwt from "jsonwebtoken";
-import {hashPassword, comparePassword} from "../utils/helpers.js"
+import { hashPassword, comparePassword } from "../utils/helpers.js";
 import userModel from "../models/userModel.js";
 
-export const userLogin = () => {};
+export const userLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userModel.findOne({ username });
+  if (!user) return res.json({ message: "Username or Password is incorrect" });
+
+  const isValid = comparePassword(password, user.password);
+  if (isValid) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token, userID: user._id });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Username or Password is incorrect" });
+  }
+};
 
 export const userRegistration = async (req, res) => {
   const { username } = req.body;
@@ -12,7 +26,6 @@ export const userRegistration = async (req, res) => {
     return res.status(400).json({ message: "User already exists!" });
   } else {
     const password = hashPassword(req.body.password);
-    console.log(password);
     await userModel.create({ password, username });
     res.sendStatus(201);
   }
