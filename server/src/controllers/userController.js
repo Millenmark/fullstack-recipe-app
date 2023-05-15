68ee50f0667d447cb6bf14f2518219c47a347bb1
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-15 11:00:48 +0800

    Create userController.js

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import {hashPassword, comparePassword} from "../utils/helpers.js"
+import userModel from "../models/userModel.js";
+
+export const userLogin = () => {};
+
+export const userRegistration = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await userModel.findOne({ username });
+  if (user) {
+    return res.status(400).json({ message: "User already exists!" });
+  } else {
+    const password = hashPassword(req.body.password);
+    console.log(password);
+    await userModel.create({ password, username });
+    res.sendStatus(201);
+  }
+};
